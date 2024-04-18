10753eb761893f390495ec368aaa0274d299c81b
DrWatt <marco.lorusso96@gmail.com>
2024-04-18 10:53:53 +0200

    input to network in the fixed step integrator and some cleanup

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import time
-# os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-# os.environ['CUDA_VISIBLE_DEVICES'] = ''
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
 import tensorflow as tf
 
 from scipy.linalg import circulant
@@ -38,27 +38,33 @@ def physical_laplacian(A, normalised = True):
   return L
 
 @tf.function
-def ode_fn(t, y, params):
+def ode_fn(t, y, x, params):
 # params = \eps, a, J (coupling strength), L (coupling matrix), I (external drivers)
   u,v = tf.split(y,2)
+  ux,vx = tf.split(x,2)
   eps, a, J, L, I = params
-  #tf.print("result of f",tf.reshape(tf.stack([(a - tf.pow(a,3)/3 - b + 0.1)/0.1,a + 0.1],axis=0),[-1]))
-  return tf.reshape(tf.stack([(u - tf.pow(u,3)/3 - v - J*tf.linalg.matvec(L,u,a_is_sparse = True) +I)/eps,
+
+  
+  return tf.reshape(tf.stack([(u - tf.pow(u,3)/3 - v - J*tf.linalg.matvec(L,u,a_is_sparse = True) +I*ux)/eps,
                                u + a],axis=0),[-1])
 
-solver = NetworkRsv("DormPrince")
-N=500
+solver = NetworkRsv("AdaptDormPrince")
+N=100
 t_init = tf.constant(0., dtype=tf.float64)
-t_max = tf.constant(10., dtype=tf.float64)
+t_max = tf.constant(1., dtype=tf.float64)
 step = tf.constant(0.001, dtype=tf.float64)
 expected_steps = 1 + int(t_max/step)
-# y_init = tf.constant([1.,1.,1.,1.,1.,1.,1.,0.,0.,0.,0.,0.], dtype=tf.float64)
+
 a = 1.3
 fp = (-a, a**3/3 - a)
 y_init = y_init = tf.constant([fp[0] + np.random.normal() for i in range(N)] + [fp[1]] * N, dtype = tf.float64)
 # y_init = tf.random.uniform([1000], dtype = tf.float64)
-atol = tf.constant(1e-4,dtype=tf.float64)
-rtol = tf.constant(1e-4,dtype=tf.float64)
+
+#    x.write(i,tf.zeros([2*N],dtype=tf.float64))
+x = tf.zeros([int(t_max/step)+1,2*N], dtype = tf.float64)
+
+atol = tf.constant(1e-3,dtype=tf.float64)
+rtol = tf.constant(1e-3,dtype=tf.float64)
 tf.print("Initial state ",y_init)
 
 A = ER_adjacency_tensor(N, 0.01, True)#tf.convert_to_tensor(circulant([0 for i in range(N - 1)] + [1]).T, dtype = tf.float64)
@@ -66,15 +72,15 @@ L = physical_laplacian(A)
 
 I = tf.constant(0., dtype = tf.float64)
 
-#params = tf.Variable([0.01, 1.3,0.5, L, I], dtype=tf.float64) 
+
 params = tf.tuple([0.01, a, 0.5, L, I]) 
 
 
-solver.fit(ode_fn,y_init,t_init,t_max, step, params)
+solver.fit(ode_fn,y_init,t_init,t_max,x, step, params)
 
 print("Starting integration")
 start_time= time.time()
-results = solver.run()
+results = solver.run(atol=atol,rtol=rtol)
 
 print("Integrated ", len(results[1]), " steps in ", time.time() - start_time, " s")
 
--- networkrsv.py
+++ networkrsv.py
@@ -14,8 +14,8 @@ class DynamicsFN:
     self.fn = fn
     self.params = args[0]
 
-  def __call__(self, y, t):
-    return self.fn(y,t,self.params)
+  def __call__(self, y, t, x):
+    return self.fn(y, t, x, self.params)
 
 
 class NetworkRsv:
@@ -30,83 +30,75 @@ class NetworkRsv:
     
     self.y0 = tf.constant([0.0],dtype=tf.float64)
     self.t0 = tf.constant([0.0],dtype=tf.float64)
+    self.x = tf.constant([0.0],dtype=tf.float64)
 
-  # @tf.function
   def set_initial_cond(self, y0, t0):
     self.y0 = y0
     self.t0 = t0
 
-  # @tf.function
+
   def set_dynamics(self, fn, fn_params):
     self.fn = DynamicsFN(fn, fn_params)
-  # @tf.function
+
   def set_integrator(self, integrator):
     self.integrator = integrator
 
-  # @tf.function
+
   def set_pace(self, t_max, step):
     self.t_max = t_max
     self.step = step
 
   @tf.function
-  def runge_kutta_dormand_prince(self, f, t0, y0, t_end, h):
-    # int((t_max - t0) / h)
+  def runge_kutta_dormand_prince(self, f, t0, y0, t_end, h, x):
     y = tf.TensorArray(dtype=tf.float64, size = 0, dynamic_size=True, clear_after_read=False)
     t = tf.TensorArray(dtype=tf.float64, size = 0, dynamic_size=True, clear_after_read=False)
     t = t.write(0,t0)
     y = y.write(0,y0)
     idx = tf.constant(0, dtype=tf.int32)
-    t.read(idx)
-    y.read(idx)
-    a = 1.3
-    fp = (-a, a**3/3 - a)
-    # pulse = tf.constant([fp[0] + 5] + [fp[0]]*(500-1) + [fp[1]] * 500, dtype = tf.float64)
-    pulse = tf.concat([tf.random.normal([500], dtype=tf.float64),tf.random.normal([500], dtype=tf.float64)],0)
-    def condition(t,y,idx):
+
+    def condition(t,y,x, idx):
       return tf.less(t.read(idx), t_end)
-    def body(t,y,idx):
+    def body(t,y,x, idx):
       y_temp = y.read(idx)
       t_temp = t.read(idx)
-      #if idx == 800:
-      #  y_temp = y_temp + pulse
-      #if idx == 1600:
-      #  y_temp = y_temp + pulse
-      #if idx == 3200:
-      #  y_temp = y_temp + pulse
-      k1 = h * f(t_temp, y_temp)
-      k2 = h * f(t_temp + h / 5, y_temp + k1 / 5)
-      k3 = h * f(t_temp + 3 * h / 10, y_temp + 3 * k1 / 40 + 9 * k2 / 40)
-      k4 = h * f(t_temp + 4 * h / 5, y_temp + 44 * k1 / 45 - 56 * k2 / 15 + 32 * k3 / 9)
-      k5 = h * f(t_temp + 8 * h / 9, y_temp + 19372 * k1 / 6561 - 25360 * k2 / 2187 + 64448 * k3 / 6561 - 212 * k4 / 729)
-      k6 = h * f(t_temp + h, y_temp + 9017 * k1 / 3168 - 355 * k2 / 33 + 46732 * k3 / 5247 + 49 * k4 / 176 - 5103 * k5 / 18656)
+      x_temp = x[idx]
+
+      k1 = h * f(t_temp, y_temp,x_temp)
+      k2 = h * f(t_temp + h / 5, y_temp + k1 / 5,x_temp)
+      k3 = h * f(t_temp + 3 * h / 10, y_temp + 3 * k1 / 40 + 9 * k2 / 40,x_temp)
+      k4 = h * f(t_temp + 4 * h / 5, y_temp + 44 * k1 / 45 - 56 * k2 / 15 + 32 * k3 / 9,x_temp)
+      k5 = h * f(t_temp + 8 * h / 9, y_temp + 19372 * k1 / 6561 - 25360 * k2 / 2187 + 64448 * k3 / 6561 - 212 * k4 / 729,x_temp)
+      k6 = h * f(t_temp + h, y_temp + 9017 * k1 / 3168 - 355 * k2 / 33 + 46732 * k3 / 5247 + 49 * k4 / 176 - 5103 * k5 / 18656,x_temp)
 
       y_temp = y_temp + 35 * k1 / 384 + 500 * k3 / 1113 + 125 * k4 / 192 - 2187 * k5 / 6784 + 11 * k6 / 84
-      # tf.print("y_temp", y_temp, "idx",idx)
       idx += 1
       y = y.write(idx, y_temp)
 
       t = t.write(idx, t_temp + h)
       tf.print("Time step", tf.cast(t_temp, tf.int32), " out of ", t_end, end='\r')
-      return t, y, idx
+      return t, y, x, idx
     
 
-    t, y, idx = tf.while_loop(condition, body, [t, y, idx],parallel_iterations=100)
+    t, y, x, idx = tf.while_loop(condition, body, [t, y, x, idx],parallel_iterations=100)
     return y.stack(),t.stack()
 
   @tf.function
-  def adaptive_runge_kutta_dormand_prince(self, f, t0, y0, t_end, h, atol, rtol):
+  def adaptive_runge_kutta_dormand_prince(self, f, t0, y0, t_end, h, x, atol, rtol):
     y = tf.TensorArray(dtype=tf.float64, size = 0, dynamic_size=True, clear_after_read=False)
     t = tf.TensorArray(dtype=tf.float64, size = 0, dynamic_size=True, clear_after_read=False)
     t = t.write(0,t0)
     y = y.write(0,y0)
+    x_temp = tf.constant([0.0,0.0], dtype=tf.float64)
     idx = tf.constant(0, dtype=tf.int32)
+    if tf.less(atol,h) or tf.less(rtol,h):
+      tf.print("Warning! Either the relative or absolute error tolerance is smaller than the initial step, the integration might not proceed")
     def step_integ(f,t,y,h):
-      k1 = h * f(t, y)
-      k2 = h * f(t + h / 5, y + k1 / 5)
-      k3 = h * f(t + 3 * h / 10, y + 3 * k1 / 40 + 9 * k2 / 40)
-      k4 = h * f(t + 4 * h / 5, y + 44 * k1 / 45 - 56 * k2 / 15 + 32 * k3 / 9)
-      k5 = h * f(t + 8 * h / 9, y + 19372 * k1 / 6561 - 25360 * k2 / 2187 + 64448 * k3 / 6561 - 212 * k4 / 729)
-      k6 = h * f(t + h, y + 9017 * k1 / 3168 - 355 * k2 / 33 + 46732 * k3 / 5247 + 49 * k4 / 176 - 5103 * k5 / 18656)
+      k1 = h * f(t, y, x_temp)
+      k2 = h * f(t + h / 5, y + k1 / 5, x_temp)
+      k3 = h * f(t + 3 * h / 10, y + 3 * k1 / 40 + 9 * k2 / 40, x_temp)
+      k4 = h * f(t + 4 * h / 5, y + 44 * k1 / 45 - 56 * k2 / 15 + 32 * k3 / 9, x_temp)
+      k5 = h * f(t + 8 * h / 9, y + 19372 * k1 / 6561 - 25360 * k2 / 2187 + 64448 * k3 / 6561 - 212 * k4 / 729, x_temp)
+      k6 = h * f(t + h, y + 9017 * k1 / 3168 - 355 * k2 / 33 + 46732 * k3 / 5247 + 49 * k4 / 176 - 5103 * k5 / 18656, x_temp)
 
       y_next = y + 35 * k1 / 384 + 500 * k3 / 1113 + 125 * k4 / 192 - 2187 * k5 / 6784 + 11 * k6 / 84
 
@@ -117,7 +109,6 @@ class NetworkRsv:
     def body(t,y,idx,h, atol, rtol):
       y_temp = y.read(idx)
       t_temp = t.read(idx)
-
       y1 = step_integ(f, t_temp, y_temp, h)
       y2 = step_integ(f, t_temp + h / 2, y1, h / 2)
       # Error estimation
@@ -136,24 +127,23 @@ class NetworkRsv:
 
       else:
         h = h_next
-      tf.print("Time step", tf.cast(t_temp, tf.int32), " out of ", t_end, "(iteration index: ", idx,")", end='\r')
+      tf.print("Time step", t_temp, " out of ", t_end, "(iteration index: ", idx,")", end='\r')
       return t, y, idx,h, atol, rtol
 
     
-    t, y, idx,_,_,_ = tf.while_loop(condition, body, [t, y, idx,h, atol, rtol],parallel_iterations=10)
+    t, y, idx,_,_,_ = tf.while_loop(condition, body, [t, y, idx, h, atol, rtol],parallel_iterations=10)
     return y.stack(),t.stack()
 
-  # @tf.function
-  def fit(self, fn, y0, t0, t_max, step, fn_params = None, integrator_params = None):
+  def fit(self, fn, y0, t0, t_max, x, step, fn_params = None, integrator_params = None):
     self.set_initial_cond(y0,t0)
     self.set_pace(t_max, step)
     self.set_dynamics(fn, fn_params) 
-    self.integrator_params = integrator_params
-  
-  # @tf.function
+    self.integrator_params = integrator_params # Unused member for now - Passing integrator parameters directly in the run method.
+    self.x = x
+
   def run(self, **kwargs):
     if self.fn == None:
       print("Function describing dynamics not found - Run `fit` before this function")
       return 1
-    return self.integrator(self.fn, self.t0, self.y0, self.t_max, self.step, **kwargs)
+    return self.integrator(self.fn, self.t0, self.y0, self.t_max, self.step,self.x, **kwargs)
 
